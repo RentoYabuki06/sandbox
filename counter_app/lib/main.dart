3aa4932a8c46674cc94ff1da6f047d3d86509ac6
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-16 14:37:40 +0900

    modified:main

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,125 +1,128 @@
 import 'package:flutter/material.dart';
+// StatefulWidgetを使用してカウンターアプリを作成
 
-void main() {
-  runApp(const MyApp());
-}
+// StatelessWidget：一度描画されると変化しない。常に同じ表示をするテキストやアイコンなど。
+// StatefullWidget：ユーザの操作などによって変化する。ボタンを押したりスクロールすることで表示が変化。
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+// StatefulWidgetクラスは外見や配置など静的な情報を保持
+// Stateクラスはウィジェットの状態を保持。再描画される内容を定義。
+// ボタンが押される度にcreateState()メソッドを呼び出し、新しいStateオブジェクトを作成
+class CounterApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _CounterAppState createState() => _CounterAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// 内部状態を管理するクラス(=プライベート)のため、アンダーバーから始める
+class _CounterAppState extends State<CounterApp> {
+  int _counter = 0; // カウンターの初期値
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
+  // カウンターをインクリメントする関数
+  // setState関数：ウィジェットの状態変化をフレームワークに通知
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  // カウンターをデクリメントする関数
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
+  // BuildContextはウィジェットツリー内の特定の位置を表す
+  // ウィジェットは階層構造になっているので、そのナビゲートの手段。
+  // BuildContext context : buildメソッド内で現在のウィジェットのビルドコンテキストへアクセスしている
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      // homeプロパティはウィジェットツリーのトップ
+      // メイン画面、つまり初期画面が指定されることが多い
+      home: Scaffold(
+        // AppBarにアプリの名前を表示
+        // appBarプロパティにはAppBarウィジェットが指定される
+        appBar: AppBar(
+          title: const Text('Counter App'),
+        ),
+        // アプリの要素（Centerで全て横方向の中央揃え）
+        body: Center(
+          child: Column(
+            // 縦方向の中央揃え
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Counter:',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              Text(
+                // "_counter"という変数の表示
+                '$_counter',
+                style: TextStyle(fontSize: 40.0),
+              ),
+              // 高さ20pxの余白生成
+              SizedBox(height: 20.0),
+              Row(
+                // 横方向の中央揃え（これ書かないと左詰めになる）
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  //インクリメント用のボタン
+                  ElevatedButton(
+                    onPressed: _incrementCounter,
+                    child: Icon(Icons.add),
+                  ),
+                  SizedBox(width: 20.0),
+                  // デクリメント用のボタン
+                  ElevatedButton(
+                    onPressed: _decrementCounter,
+                    child: Icon(Icons.remove),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+void main() {
+  runApp(CounterApp());
+}
+
+// // StatelessWidgetを継承した新しいクラスの作成
+// class PrintHelloWorld extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('MyStatelessWidget'),
+//         ),
+//         body: const Center(
+//           child: Text(
+//             'Hello World!',
+//             style: TextStyle(fontSize: 24.0),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(PrintHelloWorld());
+// }
+
+// //"Hello,worldの出力"
+// void main() {
+//   const print_helloworld = MaterialApp(
+//     home: Scaffold(
+//       body: Center(
+//         child: Text('Hello, world'),
+//       ),
+//     ),
+//   );
+//   runApp(print_helloworld);
+// }
