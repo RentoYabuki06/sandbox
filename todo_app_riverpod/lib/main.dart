d924c7834b7683cb431ca18eacb8327dfcefba88
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-18 13:05:28 +0900

    modified:riverpods

--- todo_app_riverpod/lib/main.dart
+++ todo_app_riverpod/lib/main.dart
@@ -1,125 +1,153 @@
 import 'package:flutter/material.dart';
+// riverpodsを使用するためのパッケージ
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+// 時間表示を変更するためのpackage
+import 'package:intl/intl.dart';
 
-void main() {
-  runApp(const MyApp());
+// Todoのデータモデル（変化なし）
+class Todo {
+  final String text;
+  final DateTime addedDate;
+  bool isCompleted;
+
+  Todo({
+    required this.text,
+    required this.addedDate,
+    this.isCompleted = false,
+  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// RiverpodのProviderを使ってTodoのリストを管理するProvider
+// refパラメータを受け取り、todoリストの初期値を返す
+final todoListProvider = StateProvider<List<Todo>>((ref) => []);
 
-  // This widget is the root of your application.
+void main() {
+  runApp(
+    ProviderScope(
+      child: const MaterialApp(
+        home: TodoApp(),
+      ),
+    ),
+  );
+}
+
+// StatelessWidgetでUIを作成
+class TodoApp extends StatelessWidget {
+  // スーパーコンストラクタに直接keyパラメータを追加
+  const TodoApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TodoApp_riverpods'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Column(
+        children: [
+          // 入力フィールドのUIウィジェット
+          Expanded(
+            child: TodoInputField(),
+          ), // todolistを表示している部分のUIウィジェット
+          Expanded(
+            child: TodoList(),
+          ), // todolistを表示している部分のUIウィジェット
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+// 入力フィールドのUI
+class TodoInputField extends ConsumerWidget {
+  // スーパーコンストラクタに直せkeyパラメータを追加
+  const TodoInputField({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final TextEditingController textController = TextEditingController();
+
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: textController,
+              decoration: InputDecoration(
+                hintText: 'Enter new todo',
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: Icon(Icons.clear),
+                border: OutlineInputBorder(),
+              ),
+              onSubmitted: (value) {
+                ref.read(todoListProvider).add(
+                      Todo(
+                        text: value,
+                        addedDate: DateTime.now(),
+                      ),
+                    );
+                textController.clear();
+              },
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              ref.read(todoListProvider).add(
+                    Todo(
+                      text: textController.text,
+                      addedDate: DateTime.now(),
+                    ),
+                  );
+              textController.clear();
+            },
+            child: const Text('Add todo'),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+// todolisetのUI（変化する部分なのでConsumerWidgetで作成)
+class TodoList extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todos = ref.watch(todoListProvider);
+
+    return ListView.builder(
+      itemCount: todos.length,
+      itemBuilder: (context, index) {
+        String formattedDate =
+            DateFormat('yyyy年MM月dd日 HH:mm').format(todos[index].addedDate);
+        return Dismissible(
+          key: ValueKey(todos[index]),
+          direction: DismissDirection.startToEnd,
+          onDismissed: (direction) {
+            ref.read(todoListProvider).removeAt(index);
+          },
+          background: Container(
+            color: Colors.red,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            alignment: Alignment.centerLeft,
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          child: CheckboxListTile(
+            title: Text(todos[index].text),
+            subtitle: Text(formattedDate),
+            value: todos[index].isCompleted,
+            onChanged: (newValue) {
+              ref.read(todoListProvider)[index].isCompleted = newValue!;
+            },
+            secondary: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                ref.read(todoListProvider).removeAt(index);
+              },
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        );
+      },
     );
   }
 }
